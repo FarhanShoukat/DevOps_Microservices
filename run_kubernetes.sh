077319d8df062224a0d9d019e030fd2cd3a77c79
FarhanShoukat <farhanshoukat04@icloud.com>
2022-03-24 22:04:40 +0500

    Dockerfile created. Run scripts created. docker and kubernetes outputs added.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
+dockerpath=farhan04/predict-housing-prices
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy predict-housing-prices --image=$dockerpath:latest
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/predict-housing-prices-9448c4db6-48l5k --address 0.0.0.0 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,12 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=farhan04/predict-housing-prices
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag predict-housing-prices:latest $dockerpath:latest
 
-# Step 3:
-# Push image to a docker repository
+docker push $dockerpath:latest
